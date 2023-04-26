00d3ae62837c452cb53ff710a1dbe30bc8aa9e76
Shang <shang801104@gmail.com>
2023-04-26 22:49:55 +0800

    點擊圖片變化

--- src/views/front/ProductView.vue
+++ src/views/front/ProductView.vue
@@ -10,7 +10,7 @@
                   課程內容&ensp;>&ensp;
                 </router-link>
               </li>
-              <li class="bg-white text-black rounded-1 px-1">{{ products.title }}</li>
+              <li class="bg-white text-black rounded-1 px-1">{{ title }}</li>
             </ul>
           </div>
         </div>
@@ -18,7 +18,7 @@
       <div class="row text-white pb-5">
         <div class="col-12 col-xl-6">
           <div class="card">
-            <img :src="products.imageUrl" style="height:400px;background-repeat:no-repeat;background-size:100% 100%" alt="課程圖片">
+            <img :src="`${imgUrl}`" style="height:400px;background-repeat:no-repeat;background-size:100% 100%" alt="課程圖片">
           </div>
         </div>
         <div class="col-12 col-xl-6 d-flex flex-column justify-content-between">
@@ -29,12 +29,24 @@
           <div class="d-flex flex-column align-items-end me-10 mb-10">
             <div class="fs-7" v-if="products.origin_price === products.price">原價:{{ products.origin_price }}</div>
             <div v-else>
-              <div class="fs-4">原價:{{ products.origin_price }}</div>
-              <del class="fs-7">特價{{ products.price}}</del>
+              <del class="fs-7">原價{{ products.origin_price}}</del>
+              <div class="fs-4">特價:{{ products.price }}</div>
             </div>
           </div>
         </div>
       </div>
+      <div class="row">
+        <div class="col-12">
+          <div class="d-flex">
+            <div class="product">
+              <a class="me-3" href="#" v-for="item,index in imagesUrl" :key="`${index}`" @click.prevent="subImg(item,index)">
+                <img v-if="item" :src="item" style="height:100px;width:100px;" alt="產品圖片" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -44,16 +56,24 @@ export default {
   data() {
     return {
       products: {},
+      imgUrl: '',
+      imagesUrl: [],
+      title: '',
     }
   },
   methods: {
+    subImg(item, index) {
+      this.imgUrl = item;
+    },
     getProduct() {
       const { id } = this.$route.params //從網址取得id
       this.$http
         .get(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/product/${id}`)
         .then((res) => {
           this.products = res.data.product
-          console.log('取得客戶產品', res.data.product);
+          this.title = res.data.product.title
+          this.imgUrl = res.data.product.imageUrl
+          this.imagesUrl = res.data.product.imagesUrl
         })
     }
   },
